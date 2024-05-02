30e5439f717bb07286b2d9891ec8d2d1ce4ca9e3
tessaag <tessageorge.2003@gmail.com>
2024-05-02 11:48:52 +0530

    about page added

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,24 @@
-'use client'
-import React from "react";
-import { useEffect } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import Link from 'next/link';
 
-
-
-export default function App() {
-  const router = useRouter()
-  usePathname();
-  useEffect(()=>{
-      router.push('/home')   
-  },[]);
+export default function Home() {
   return (
-    <>
-    </>
+    <div>
+      <nav className="text-white py-4">
+      <div className="flex justify-center">
+        <Link href="/" legacyBehavior>
+          <a className="mx-4 text-lg md:text-xl lg:text-2xl font-bold border-b-2 border-white levenim-mt" style={{ fontSize: "40px", lineHeight: "56px", fontWeight: "bold" }}>Home</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a className="mx-4 text-lg md:text-xl lg:text-2xl font-normal levenim-mt" style={{ fontSize: "40px", lineHeight: "56px" }}> About </a>
+        </Link>
+        <Link href="/services" legacyBehavior>
+          <a className="mx-4 text-lg md:text-xl lg:text-2xl font-normal levenim-mt" style={{ fontSize: "40px", lineHeight: "56px" }}>Services</a>
+        </Link>
+        <Link href="/careers" legacyBehavior>
+          <a className="mx-4 text-lg md:text-xl lg:text-2xl font-normal levenim-mt" style={{ fontSize: "40px", lineHeight: "56px" }}>Careers</a>
+        </Link>
+      </div>
+  </nav>
+    </div>
   );
 }
